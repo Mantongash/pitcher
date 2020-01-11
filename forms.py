97b0790fb2c7a1fb732ba78313925ec6c7c0840f
Mantongash <you@example.com>
2020-01-12 01:35:53 +0300

    Created a username property

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 
+
 class RegistrationForm(FlaskForm):
-  
+    username = StringField("Username", validators=[DataRequired()])
