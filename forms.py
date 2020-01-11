4f7dda1741614e551240b7e5dee9f29b73e1962d
Mantongash <you@example.com>
2020-01-12 01:42:58 +0300

    Created a password property

--- forms.py
+++ forms.py
@@ -6,3 +6,4 @@ from wtforms.validators import DataRequired, Email
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
