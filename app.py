b88f4870a1b97b4b98075b1a61a8a44b88a109f5
Mantongash <you@example.com>
2020-01-13 20:38:06 +0300

    Moved all the imports, app initialization and db into the __init__.py file

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-from datetime import datetime
-from flask import Flask, render_template, redirect, flash,url_for
-from forms import RegistrationForm, LoginForm
-from flask_sqlalchemy import SQLAlchemy
 
-
-app = Flask(__name__)
-
-app.config["SECRET_KEY"] = "6b2d9404ac5ccfb7635f8bc5650b5119"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///records.db"
-
-db = SQLAlchemy(app)
 
 
 
---
+++ app/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(20), nullable=False, unique=True)
+    img_file = db.Column(db.String(20), default="default.jpg")
+    password = db.Column(db.String(60),  nullable=False)
+    posts = db.relationship("Pitch", backref="author", lazy=True)
+
+    def __repl__(self):
+        return f"User({self.username}, {self.email}, {self.img_file})"
+
+class Pitch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+    def __repl__(self):
+        return f"Pitch({self.title}, {self.date_posted}, {self.img_file})"
