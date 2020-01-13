41c35e05cf00f3461fe45788b9d9be7edec1a380
Mantongash <you@example.com>
2020-01-13 20:27:25 +0300

    Added a models file

--- app.py
+++ app.py
@@ -11,26 +11,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///records.db"
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    email = db.Column(db.String(20), nullable=False, unique=True)
-    img_file = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(60), nullable=False, default="default.jpg")
-    posts = db.relationships("Pitch", backref="author", lazy=True)
-
-    def __repl__(self):
-        return f"User({self.username}, {self.email}, {self.img_file})"
-
-class Pitch(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-
-
-    def __repl__(self):
-        return f"Pitch({self.title}, {self.date_posted}, {self.img_file})"
 
 
 @app.route("/")
