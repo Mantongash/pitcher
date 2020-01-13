d12106328f0d2264f8e094946503531b68cdd5df
Mantongash <you@example.com>
2020-01-13 16:39:13 +0300

    Imported datetime

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, redirect, flash,url_for
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
