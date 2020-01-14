e9589a643cef870c645e3b9d67012ea8cbfb7b1a
Mantongash <you@example.com>
2020-01-14 07:50:57 +0300

    Created an instance of Bcrypt

--- pitcher/__init__.py
+++ pitcher/__init__.py
@@ -10,5 +10,6 @@ app.config["SECRET_KEY"] = "6b2d9404ac5ccfb7635f8bc5650b5119"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///records.db"
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pitcher import routes
